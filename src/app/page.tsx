63f180099b4c47dac108af8f3a86d83c04fe10c7
Kevin Atsma <kevinatsma@vivatranslate.com>
2024-12-12 22:40:43 +0100

    Reset --- use next default

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,15 @@
-"use client";
-import { useEffect, useState } from "react";
+import Image from "next/image";
 
 export default function Home() {
-
-  useEffect(() => {
-    if ('scrollRestoration' in history) {
-      history.scrollRestoration = 'manual';
-    }
-  }, []);
-
   return (
-    <main className="bg-black flex w-full h-full items-center justify-center" id="home-page">
-      RED
-    </main>
-  )
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-full">
+        <h2 className="text-center text-4xl w-full">
+          La MF
+        </h2>
+        <h1 className="text-center text-8xl text-bold w-full">RED</h1>
+        <h2 className="text-center text-4xl w-full">In tha HOUSE</h2>
+      </main>
+    </div>
+  );
 }
